abaa7a176a3fc4d409807cef7ea5464aaeb56af3
sharan.karanth@gmail.com <sharan.karanth@gmail.com>
2013-01-22 08:44:22 -0500

    Initial Commit

---
+++ RotateCubeSpike/Shaders/Shader.fsh
@@ -0,0 +1,14 @@
+//
+//  Shader.fsh
+//  RotateCubeSpike
+//
+//  Created by skaranth on 1/22/13.
+//  Copyright (c) 2013 skaranth. All rights reserved.
+//
+
+varying lowp vec4 colorVarying;
+
+void main()
+{
+    gl_FragColor = colorVarying;
+}
---
+++ RotateCubeSpike/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RotateCubeSpike
+//
+//  Created by skaranth on 1/22/13.
+//  Copyright (c) 2013 skaranth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
